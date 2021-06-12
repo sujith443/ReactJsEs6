83f3eeb960c0b3acf94e4b5a46a1dd94466256ed
sujith <psujith901.sm@gmail.com>
2021-06-12 12:00:57 +0530

    Mapfunctionpractise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+import emoji from "./emojipedia";
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
@@ -9,3 +11,10 @@ var numbers = [3, 56, 2, 48, 5];
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
+
+{
+  emoji.map(function (x) {
+    let hundred = x.meaning.substr(0, 100);
+    console.log(hundred);
+  });
+}
